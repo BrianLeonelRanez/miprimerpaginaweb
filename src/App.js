11f0240221bef90d5c3042da52538e5830ba5f6f
BrianLeonelRanez <brian.ranez@gmail.com>
2020-08-12 23:39:21 -0300

    Agregue mi nombre

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Brian Leonel Rañez
         </a>
       </header>
     </div>
